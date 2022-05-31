52e849141c4a2d8ead6e2af0406ba7589ef79419
Dmitry Krayniy <krdima92@gmail.com>
2022-05-31 10:33:04 +0300

    Updated release script

--- release.ps1
+++ release.ps1
@@ -12,13 +12,19 @@ if ($component.version -ne $package.version) {
     throw "Versions in component.json and package.json do not match"
 }
 
-# Automatically login to npm registry
+# Automatically login to npm registry by npm-cli-login
 if ($env:NPM_USER -ne $null -and $env:NPM_PASS -ne $null -and $env:NPM_EMAIL -ne $null) {
     if (npm whoami -ne $env:NPM_USER) {
-        Write-Host "Logging to npmjs registry..."
+        Write-Host "Logging to npmjs registry by npm-cli-login..."
         npm-cli-login
     }
 } 
+
+# Automatically login to npm registry by putting token to .npmrc
+if ($env:NPM_TOKEN -ne $null) {
+    Write-Host "Creating and using project scope .npmrc"
+    Set-Content -Path ".npmrc" -Value "//registry.npmjs.org/:_authToken=${NPM_TOKEN}"
+}
 
 Write-Host "Getting $($package.name) versions from npm registry..."
 
